2ed9bf2ac3bdf599268c6e744bf831fb3b584a8d
yongho <yongho73@gmail.com>
2024-10-01 18:28:44 +0900

    수정.테스트

--- v1/api/src/main/java/com/noah/api/ApiApplication.java
+++ v1/api/src/main/java/com/noah/api/ApiApplication.java
@@ -7,9 +7,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @EnableConfigurationProperties
 @SpringBootApplication
 public class ApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
-
 }
